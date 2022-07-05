654628b68f21f36e728b8c31a8f1303263b327a2
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2022-07-05 12:31:09 +0000

    feat(lov): add metadata generation for publishing with LOV (non-normative)

--- bin/gen-trig.py
+++ bin/gen-trig.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Generate concatenated TriG files."""
 # COPYRIGHT 2021 Redmer Kronemeijer <redmer.kronemeijer@crow.nl>
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #       http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -66,6 +66,20 @@ def generate_triple_files():
     g.serialize("./data/concat/nen2660.json", format="json-ld", encoding="utf-8")
 
 
+def generate_lov_files():
+    g = Graph()
+
+    for input_file in Path("./data/").glob("*.ttl"):
+        g.parse(str(input_file), format=input_file.suffix[1:])
+
+    g.parse("./data/metadata/lov.ttl", format="ttl")
+
+    g.serialize(
+        "./data/concat/nen2660-lov-metadata.ttl", format="ttl", encoding="utf-8"
+    )
+
+
 if __name__ == "__main__":
     generate_quad_files()
     generate_triple_files()
+    generate_lov_files()
